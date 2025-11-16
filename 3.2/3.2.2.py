f4e9d307b8016912f7aa41d01bef79c09ea1e26a
Vladyslav Melnychenko <vldslv.melnychenko@gmail.com>
2025-11-16 14:24:20 +0200

    Final work on data input/output

---
+++ 3.2/3.2.2.py
@@ -0,0 +1,6 @@
+a = int(input())
+b = int(input())
+v = (a + b) ** 2
+s = a ** 2 + b ** 2
+print('Square of sum', a, 'and', b, 'equal to', v)
+print('Sum of squares', a, 'and', b, 'equal to', s)
---
+++ 3.2/3.2.4.py
@@ -0,0 +1,4 @@
+x = int(input())
+xx = (10 * x) + x
+xxx = (100 * x) + xx
+print(x + xx + xxx)
